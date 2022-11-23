34b30a298dcec650ba9fe8621491cac5a23533a2
unknown <dollorinfotech@gmail.com>
2022-11-23 10:01:30 +0530

    1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+// Global Variable
+
+let h2 = document.getElementById('myheading2');
+function changeHeading2(){
+    //Local Variable
+    
+    h2.innerHTML = 'DOLLORINFOTECH';
+}
+
+
+
+
+
---
+++ script3.js
@@ -0,0 +1,14 @@
+
+// Global Variable
+
+let h3 = document.getElementById('myheading3');
+function changeHeading3(){
+    //Local Variable
+    
+    h3.innerHTML = 'DOLLORINFOTECH3';
+}
+
+
+
+
+
